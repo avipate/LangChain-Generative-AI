2282b68d128bd39cd7ab1bf50027242594cff563
Avi Patel <avip1607@outlook.com>
2023-10-05 17:19:04 +0530

    Change: To clean the unnecessary data from json

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -20,12 +20,10 @@ def scrape_linkedin_profile(linkedin_profile_url: str):
         k: v
         for k, v in data.items()
         if v not in ([], "", "", None)
-           and k not in ['people_also_viewed', "certifications"]
+        and k not in ["people_also_viewed", "certifications"]
     }
     if data.get("groups"):
         for group_dict in data.get("groups"):
             group_dict.pop("profile_pic_url")
 
     return data
-
-
