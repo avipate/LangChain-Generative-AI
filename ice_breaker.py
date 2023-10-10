fccfbd7a9bb1c0d182feb613fb7185e8a3de5994
Avi Patel <avip1607@outlook.com>
2023-10-10 17:27:30 +0530

    Reformat the Class LangChainModel into a Python function named ice_break

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,4 +1,6 @@
 # Importing required libraries
+from typing import Tuple
+
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
@@ -11,55 +13,39 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 load_dotenv(find_dotenv())
 
 
-class LangChainLLM:
-    def __init__(self):
-        self.summary_template = """
-            given the Linkedin information {information} about a person from I want you to create:
-            1. short description
-            2. two interesting facts about them
-            3. A topic that may interest them
-            4. 2 creative Ice Breaker to open a conversation with them
-            \n{format_instructions} 
-        """
-        self.chain, self.llm, self.summary_prompt_template, self.format_instruction = (
-            None,
-            None,
-            None,
-            None,
-        )
-        name = None
+# Function to integrate LangChain with OpenAI gpt model and get data from LinkedIn
+def ice_break(name: str) -> Tuple[PersonalIntel, str]:
+    # LinkedIn URL
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    # get the linkedin data
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
-    def prompts(self):
-        self.summary_prompt_template = PromptTemplate(
-            input_variables=["information"],
-            template=self.summary_template,
-            partial_variables={
-                "format_instructions": person_intel_parser.get_format_instructions()
-            },
-        )
+    summary_template = """
+                given the Linkedin information {information} about a person from I want you to create:
+                1. short description
+                2. two interesting facts about them
+                3. A topic that may interest them
+                4. 2 creative Ice Breaker to open a conversation with them
+                \n{format_instructions} 
+            """
 
-    def llm_model(self, name) -> PersonalIntel:
-        self.prompts()
-        # Initialize the ChatOpenAI model
-        self.llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
+    )
+    # Initialize the ChatOpenAI model
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+    # Build a chain to link the llm model and prompts
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-        # Build a chain to link the llm model and prompts
-        chain = LLMChain(llm=self.llm, prompt=self.summary_prompt_template)
+    result = chain.run(information=linkedin_data)
 
-        # LinkedIn URL
-        linkedin_profile_url = linkedin_lookup_agent(name=name)
-        # get the linkedin data
-        linkedin_data = scrape_linkedin_profile(
-            linkedin_profile_url=linkedin_profile_url
-        )
-
-        result = chain.run(information=linkedin_data)
-
-        return person_intel_parser.parse(result)
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
     print("Hello LangChain!")
-
-    IceBreaker = LangChainLLM()
-    IceBreaker.llm_model(name="Avi Patel")
+    ice_break(name="Avi Patel")
