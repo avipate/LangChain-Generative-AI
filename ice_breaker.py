c33714cd91aa50b8f61b027db2c3a3ebadba08ec
Avi Patel <avip1607@outlook.com>
2023-10-10 17:16:05 +0530

    Add: Output Parser to get the data ready for the frontend integration

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,7 +4,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv, find_dotenv
 from third_parties.linkedin import scrape_linkedin_profile
-# from third_parties.twitter import scrap_user_tweets
+from output_parser import *
+
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 load_dotenv(find_dotenv())
@@ -13,18 +14,31 @@ load_dotenv(find_dotenv())
 class LangChainLLM:
     def __init__(self):
         self.summary_template = """
-            given the Linkedin information {information} about a person from I want you tp create:
+            given the Linkedin information {information} about a person from I want you to create:
             1. short description
             2. two interesting facts about them
+            3. A topic that may interest them
+            4. 2 creative Ice Breaker to open a conversation with them
+            \n{format_instructions} 
         """
-        self.chain, self.llm, self.summary_prompt_template = None, None, None
+        self.chain, self.llm, self.summary_prompt_template, self.format_instruction = (
+            None,
+            None,
+            None,
+            None,
+        )
+        name = None
 
     def prompts(self):
         self.summary_prompt_template = PromptTemplate(
-            input_variables=["information"], template=self.summary_template
+            input_variables=["information"],
+            template=self.summary_template,
+            partial_variables={
+                "format_instructions": person_intel_parser.get_format_instructions()
+            },
         )
 
-    def llm_model(self):
+    def llm_model(self, name) -> PersonalIntel:
         self.prompts()
         # Initialize the ChatOpenAI model
         self.llm = ChatOpenAI(model_name="gpt-3.5-turbo")
@@ -33,7 +47,7 @@ class LangChainLLM:
         chain = LLMChain(llm=self.llm, prompt=self.summary_prompt_template)
 
         # LinkedIn URL
-        linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
+        linkedin_profile_url = linkedin_lookup_agent(name=name)
         # get the linkedin data
         linkedin_data = scrape_linkedin_profile(
             linkedin_profile_url=linkedin_profile_url
@@ -41,11 +55,11 @@ class LangChainLLM:
 
         result = chain.run(information=linkedin_data)
 
-        return result
+        return person_intel_parser.parse(result)
 
 
 if __name__ == "__main__":
     print("Hello LangChain!")
 
     IceBreaker = LangChainLLM()
-    IceBreaker.llm_model()
+    IceBreaker.llm_model(name="Avi Patel")
