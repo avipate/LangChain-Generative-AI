f019a0ae767b60b102dbb4b762b1b3e57c06b308
Avi Patel <avip1607@outlook.com>
2023-10-11 18:14:56 +0530

    Add: output praser to prase the output in JSON format

--- output_parser.py
+++ output_parser.py
@@ -1,30 +1,33 @@
-# Importing required libraries
-from langchain.output_parsers import PydanticOutputParser
-from pydantic import BaseModel, Field
 from typing import List
 
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
 
-# Base model from Pydantic
-class PersonalIntel(BaseModel):
-    summary: str = Field(description="Summary of the person")
-    facts: List[str] = Field(description="Interesting facts about the person")
-    topics_of_interest: List[str] = Field(
-        description="Topics that may interest the person"
-    )
-    ice_breaker: List[str] = Field(
-        description="Create ice breaker to open a conversation with the person"
-    )
 
-    # Function to serialize the code
+class Summary(BaseModel):
+    summary: str = Field(description="summary")
+    facts: List[str] = Field(description="interesting facts about them")
+
     def to_dict(self):
-        return {
-            "summary": self.summary,
-            "facts": self.facts,
-            "topics_of_interest": self.topics_of_interest,
-            "ice_breaker": self.ice_breaker,
-        }
+        return {"summary": self.summary, "facts": self.facts}
 
 
-person_intel_parser: PydanticOutputParser = PydanticOutputParser(
-    pydantic_object=PersonalIntel
-)
+class IceBreaker(BaseModel):
+    ice_breakers: List[str] = Field(description="ice breaker list")
+
+    def to_dict(self):
+        return {"ice_breakers": self.ice_breakers}
+
+
+class TopicOfInterest(BaseModel):
+    topics_of_interest: List[str] = Field(
+        description="topic that might interest the person"
+    )
+
+    def to_dict(self):
+        return {"topics_of_interest": self.topics_of_interest}
+
+
+summary_parser = PydanticOutputParser(pydantic_object=Summary)
+ice_breaker_parser = PydanticOutputParser(pydantic_object=IceBreaker)
+topics_of_interest_parser = PydanticOutputParser(pydantic_object=IceBreaker)
